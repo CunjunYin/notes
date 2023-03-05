b0781a0fc56f1e8ac6b86a7d515681ecfdf423b2
Cunjun Yin <cunjunyin@outlook.com>
2023-03-05 19:03:44 +1000

    Signletone

---
+++ DesignPatterns/Creational Patterns/Code/Singleton/Program.cs
@@ -0,0 +1,41 @@
+﻿
+namespace Singleton;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("====================");
+        Console.WriteLine("New Instance");
+        Console.WriteLine("====================");
+        newInstance();
+
+        Console.WriteLine("====================");
+        Console.WriteLine("Signleton");
+        Console.WriteLine("====================");
+        signleton();
+    }
+
+    private static void signleton()
+    {
+        SingletonDemo instance1 = SingletonDemo.GetInstance();
+        SingletonDemo instance2 = SingletonDemo.GetInstance();
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine(instance1.GetNextData());
+            Console.WriteLine(instance2.GetNextData());
+        }
+    }
+
+    private static void newInstance()
+    {
+        SingletonDemo instance1 = new SingletonDemo();
+        SingletonDemo instance2 = new SingletonDemo();
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine(instance1.GetNextData());
+            Console.WriteLine(instance2.GetNextData());
+        }
+    }
+}
---
+++ DesignPatterns/Creational Patterns/Code/Singleton/SingletonDemo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton;
+
+public class SingletonDemo
+{
+    private static SingletonDemo instance;
+    private List<string> data = new List<string>();
+    private int index = 0;
+
+    // Shoule be private in real project
+    public SingletonDemo()
+    {
+        data.Add("1");
+        data.Add("2");
+        data.Add("3");
+        data.Add("4");
+        data.Add("5");
+    }
+
+    public static SingletonDemo GetInstance()
+    {
+        if (instance == null) {
+            Console.WriteLine("Initialize Signleton");
+            instance = new SingletonDemo();
+        }
+
+        return instance;
+    }
+
+    public string GetNextData()
+    {
+        var value = data[index];
+        index = index + 1;
+        if (index == data.Count)
+        {
+            index = 0;
+        }
+        return value;
+    }
+}

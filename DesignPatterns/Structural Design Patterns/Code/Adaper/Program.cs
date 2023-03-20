759111e8c7d30500e9b4ab16e70c94c8c86ca9d5
Cunjun Yin <cunjunyin@outlook.com>
2023-03-20 22:01:14 +1000

    adapter

---
+++ DesignPatterns/Structural Design Patterns/Code/Adaper/Program.cs
@@ -0,0 +1,45 @@
+﻿public interface ITarget
+{
+    public void Targetrequest();
+}
+
+public class ConcreteTarget : ITarget
+{
+    public void Targetrequest()
+    {
+        Console.WriteLine("Target method");
+    }
+}
+
+public class Adaptee
+{
+    public void AdapteeRequest()
+    {
+        Console.WriteLine("Adaptee method");
+    }
+}
+
+public class Adapter : ITarget
+{
+    private readonly Adaptee _adaptee;
+
+    public Adapter(Adaptee adaptee)
+    {
+        _adaptee = adaptee;
+    }
+
+    public void Targetrequest()
+    {
+        _adaptee.AdapteeRequest();
+    }
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var adaptee = new Adaptee();
+        var target = new Adapter(adaptee);
+        target.Targetrequest();
+    }
+}

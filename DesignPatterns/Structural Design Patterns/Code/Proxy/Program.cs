3f20084810dc7ade1731a1e8af5abbcea851781b
Cunjun Yin <cunjunyin@outlook.com>
2023-03-28 21:58:36 +1000

    Proxy

---
+++ DesignPatterns/Structural Design Patterns/Code/Proxy/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Proxy;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Proxy proxy = new Proxy(new RealSubject());
+        proxy.Request();
+    }
+}
---
+++ DesignPatterns/Structural Design Patterns/Code/Proxy/Proxy.cs
@@ -0,0 +1,16 @@
+﻿namespace Proxy;
+
+public class Proxy : ISubject
+{
+    private RealSubject _realSubject;
+
+    public Proxy(RealSubject realSubject)
+    {
+        _realSubject = realSubject;
+    }
+
+    public void Request()
+    {
+        _realSubject.Request();
+    }
+}

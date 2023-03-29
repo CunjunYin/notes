2c95d3e7d65f345b92b72b765a974926754c01aa
Cunjun Yin <cunjunyin@outlook.com>
2023-03-29 22:43:19 +1000

    Template Methods

---
+++ DesignPatterns/Behavioral Design Patterns/Code/TemplateMethod/ConcreteClasses.cs
@@ -0,0 +1,27 @@
+﻿namespace TemplateMethod;
+
+public class ConcreteClass1 : AbstractClass
+{
+    public override void Operation1()
+    {
+        Console.WriteLine("ConcreteClass1: Operation1");
+    }
+
+    public override void Operation2()
+    {
+        Console.WriteLine("ConcreteClass1: Operation2");
+    }
+}
+
+public class ConcreteClass2 : AbstractClass
+{
+    public override void Operation1()
+    {
+        Console.WriteLine("ConcreteClass2: Operation1");
+    }
+
+    public override void Operation2()
+    {
+        Console.WriteLine("ConcreteClass2: Operation2");
+    }
+}
---
+++ DesignPatterns/Behavioral Design Patterns/Code/TemplateMethod/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace TemplateMethod;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        AbstractClass c;
+        c = new ConcreteClass1();
+        c.TemplateMethod();
+
+        c = new ConcreteClass2();
+        c.TemplateMethod();
+    }
+}

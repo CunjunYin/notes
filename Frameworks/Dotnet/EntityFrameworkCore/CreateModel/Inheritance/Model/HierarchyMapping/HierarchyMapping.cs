10a117b78218b4f9f7c0872b636b8f5e4c0ce040
Cunjun Yin <cunjunyin@outlook.com>
2023-06-07 21:40:30 +1000

    update

---
+++ Frameworks/Dotnet/EntityFrameworkCore/CreateModel/Inheritance/Model/HierarchyMapping/HierarchyMapping.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Inheritance.Model.HierarchyMapping;
+
+internal class HierarchyMapping : DbContext
+{
+    public DbSet<Blog> Blogs { get; set; }
+    public DbSet<RssBlog> RssBlogs { get; set; }
+}
+
+public class Blog
+{
+    public int BlogId { get; set; }
+    public string Url { get; set; }
+}
+
+public class RssBlog : Blog
+{
+    public string RssUrl { get; set; }
+}
---
+++ Frameworks/Dotnet/EntityFrameworkCore/CreateModel/Inheritance/README.md
@@ -0,0 +1,12 @@
+﻿# Dependency
+```sh
+dotnet add package Microsoft.EntityFrameworkCore.Design --version 7.0.5
+dotnet add package Microsoft.EntityFrameworkCore.SqlServer --version 7.0.5
+dotnet add package Microsoft.EntityFrameworkCore --version 7.0.5
+```
+
+# Migrate
+```sh
+dotnet ef migrations add Initialize
+dotnet ef database update
+```
